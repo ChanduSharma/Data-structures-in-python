5e08a90c8a5e443b0201ee1dfd157b8a9e2e8c80
Chandra Kant Sharma <chandu.rikku@gmail.com>
2018-09-20 00:11:13 +0530

    Optimized get maximum from stack

---
+++ stack/optimized_getmax.py
@@ -0,0 +1,39 @@
+# Function to get maximum from a stack in O(1) time
+import random
+
+class Maximum:
+
+	def __init__(self):
+		self.array = []
+		self.maximum = []
+
+	def is_empty(self):
+		return self.array == []
+
+	def push(self, val):
+
+		self.array.append(val)
+
+		if (self.maximum == []) or (val >= self.maximum[-1]):
+			self.maximum.append(val)
+		
+
+	def pop(self):
+
+		if not self.is_empty():
+			a = self.array.pop()
+			if a == self.maximum[-1]:
+				self.maximum.pop()
+		else:
+			return -1
+
+	def get_max(self):
+		return self.maximum[-1]
+
+x = Maximum()
+
+for i in range(10):
+	x.push(random.randint(0,100))
+
+print(*x.array)
+print(x.get_max())
